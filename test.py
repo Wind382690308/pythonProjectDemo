d2e04963e22389330950c243d2114913329443da
Wind <382690308@qq.com>
2017-04-25 00:49:10 +0800

    ..
    ..

---
+++ test.py
@@ -0,0 +1,53 @@
+# Filename : test.py
+# coding=utf-8
+
+
+print ('hello world')
+
+
+#如果你需要某个
+# Python 函数或语句的快速信息帮助，
+# 那么你可以使用内建的 help 功能。
+# 尤其在你使用带提示符的命令行的时候，
+# 它十分有用。比如，运行 help(str)——这会显示 str 类的帮助
+# help(str)
+
+
+#我几乎可以保证你在每个
+#  Python 程序中都要用到字符串，
+# 所以请特别留心下面 这部分的内容。
+# 下面告诉你如何在 Python 中使用字符串。
+print (r'what\'s your name')
+
+#自然字符串 R
+#Unicode 字符串v u
+# 字符串是不可变的
+# 按字面意义级连字符串
+
+print (r'what\'s' 'your name?')
+
+
+i=5
+print i
+i=i+1
+print i
+
+# 运算符与表达式
+# 幂运算 **
+print 3**4
+# 取整
+print 10//7
+# 取余
+print 10%7
+# 左移
+print 2<<3
+# 右移
+print 16>>3
+# 按位与
+print 5&3
+# 按位或
+print  5|3
+# 按位异或
+print  5^3
+# 按位翻转
+# not 布尔非  and 布尔与
